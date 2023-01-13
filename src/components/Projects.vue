85ae249c41a571271d568afcab6a8acb94793cfa
Asandile <asandilelangeni@gmail.com>
2023-01-13 09:46:32 +0200

    main

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,13 +1,12 @@
 <template>
     <div>
         <h1>PROJECTS</h1>
-        <div class="card" style="width: 18rem;" v-for="pro in Projects" :key="pro">
-  <div class="card-body" >
-    <img :src="pro.image" alt="">
-    <h5 class="card-title">{{pro.title}}</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <a href="#" class="card-link">{{pro.githubLink}}</a>
-    <a href="#" class="card-link">{{pro.netlifyLink }}</a>
+        <div class="card" style="width: 18rem;">
+  ``<img src="" class="card-img-top" alt="">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
     </div>
@@ -15,7 +14,7 @@
 <script>
 export default {
     name: 'Projects',
-    data(){
+data() {
         return {
             Projects:[
                 {
